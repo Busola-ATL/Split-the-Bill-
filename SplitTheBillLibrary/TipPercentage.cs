ee147d0fc19b895835f7b10111b263001179795c
Busola-ATL <156509764+Busola-ATL@users.noreply.github.com>
2024-04-18 23:19:08 +0000

    a method with two arguments: a dictionary of <String, Decimal> and a float.

---
+++ SplitTheBillLibrary/TipPercentage.cs
@@ -0,0 +1,30 @@
+﻿namespace SplitBill. SplitTheBillLibrary;
+
+public class TipPercentage{
+    public Dictionary<string, decimal> getTipPercentageCalculated(Dictionary<string, decimal> detail, float tipPercentage){
+
+        Dictionary<string, decimal> namesAndTips = new Dictionary<string, decimal>();
+
+        decimal totalMealCost = 0;
+
+        foreach(var nameTip in detail){
+            totalMealCost += nameTip.Value;
+        }
+
+        decimal totalTipAmount = totalMealCost * (decimal)(tipPercentage/100);
+
+        foreach(var single in detail){
+            decimal tipForAPerson = (single.Value / totalMealCost) * totalTipAmount;
+            namesAndTips.Add(single.Key, tipForAPerson);
+        }
+        
+        if(namesAndTips.Count == 0 ){
+            return null;
+        }
+
+        return namesAndTips;
+    }
+
+    
+
+}
